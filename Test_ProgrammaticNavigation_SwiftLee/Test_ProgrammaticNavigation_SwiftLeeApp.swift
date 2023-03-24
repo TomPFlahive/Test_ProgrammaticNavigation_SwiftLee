ef9c0072f2bb4123bb0aa9790fb2daba84e48cb5
Tom Flahive <TomPFlahive@gmail.com>
2023-03-24 15:24:46 -0700

    Initial Commit

---
+++ Test_ProgrammaticNavigation_SwiftLee/Test_ProgrammaticNavigation_SwiftLeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProgrammaticNavigation_SwiftLeeApp.swift
+//  Test_ProgrammaticNavigation_SwiftLee
+//
+//  Created by Tom Flahive on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProgrammaticNavigation_SwiftLeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
